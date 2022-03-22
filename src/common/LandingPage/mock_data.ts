7093633ebb1a0f060bf4bc281349c858fbd3b8d9
Marnien Cueba <marnien@dnamicro.com>
2022-03-22 17:04:45 +0800

    update date

--- src/common/LandingPage/mock_data.ts
+++ src/common/LandingPage/mock_data.ts
@@ -1,30 +1,37 @@
+
 import profileImage from '../../assets/img/MarnienCueva2.png'
 
-const mock_data = [
-  {
-    id: 'landing',
-    greeting: 'Hi, I am',
-    name: 'Marnien Cueba',
+const mock_data = {
+  id: 'landing',
+  greeting: 'Hi, I am',
+  name: 'Marnien Cueba',
+  description1: `Currently working as a Team Lead UI Designer at DNA Micro
+  Software, Inc. Quick learner and love to explore new ideas
+  and technologies. My key skills include: UI designing, Responsive
+  Design, Wireframing using Mockflow. And love to explore UX for
+  prototyping using Figma.`,
+  description2: `My passion in designing is something I do in my free time which is
+  the main reason that I spend hours creating spikes for future
+  development.`,
+  actions: [
+    {
+      id: 'hire-me',
+      label: 'Hire Me',
+      color: 'primary',
+      icon: '',
+      onClick: () => {},
+    },
+    {
+      id: 'portfolio',
+      label: 'Portfolio',
+      color: 'primary',
+      icon: '',
+      onClick: () => {},
+    }
+  ],
+  image: {
     alt: 'Marnien Cueba profile',
-    image: profileImage,
-    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam
-    sint saepe molestiae perspiciatis aspernatur ullam, dolores ipsa
-    beatae pariatur debitis? Enim nihil quam quaerat velit quas,
-    assumenda veritatis quis repellendus.`,
-    actions: [
-      {
-        id: 'hire-me',
-        label: 'Hire Me',
-        icon: '',
-        onClick: () => {},
-      },
-      {
-        id: 'portfolio',
-        label: 'Portfolio',
-        icon: '',
-        onClick: () => {},
-      }
-    ]
+    src: profileImage,
   }
-]
+}
 export default mock_data
