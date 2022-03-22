9fc066211bb6d63b846f09e170f36f8b50e7a30a
Marnien Cueba <marnien@dnamicro.com>
2022-03-22 17:05:13 +0800

    Button: initial commit

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,15 +4,19 @@ import { IButtonProps } from './types'
 import { ThemeContext } from '../../context/ThemeContext'
 
 const Button: React.FunctionComponent<IButtonProps> = (props) => {
-  const { className, label } = props
+  const { className, children, color = 'primary', label } = props
 
   const {
     theme: {
-      buttonStyle: { baseCls },
+      buttonStyle: { baseCls, colorCls },
     },
   } = useContext(ThemeContext)
 
-  return <button className={cn('Button', baseCls, className)}>{label}</button>
+  return (
+    <button className={cn('Button', baseCls, colorCls[color], className)}>
+      {label || children}
+    </button>
+  )
 }
 
 export default Button
--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 export interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  className?: string,
+  className?: string
+  children?: ReactNode
   label?: string
+  color?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'black' | 'white' | 'transparent'
 }
