856f2aa6e4b204ac08b40f6ca36a8fb3e46221f1
Marnien Cueba <marnien@dnamicro.com>
2022-03-22 17:03:02 +0800

    Image: enhancement

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,19 +1,135 @@
-import React from 'react'
+import React, { useContext, useMemo } from 'react'
 import cn from 'classnames'
 import { Img } from 'react-image'
 import { IImageProps } from './types'
+import { ThemeContext } from '../../context/ThemeContext'
+import { Icon, Spinner } from '../index'
 
-const Image: React.FunctionComponent<IImageProps> = (props) => {
-  const { className, alt, src, loader, unloader } = props
+const Image: React.FC<IImageProps> = (props) => {
+  const {
+    className,
+    border = true,
+    background = 'default',
+    alt,
+    src,
+    size = 'xl',
+    fit = 'cover',
+    ratio = '16:9',
+    rounded = 'unset',
+    width,
+    height,
+    loader = <Spinner />,
+    unloader = <Icon />,
+  } = props
 
-  return (
+  const {
+    theme: {
+      imageStyle: {
+        baseCls,
+        roundedCls,
+        containerCls,
+        overlayCls,
+        imgCls,
+        fitCls,
+        borderCls,
+        backgroundCls,
+      },
+    },
+  } = useContext(ThemeContext)
+
+  // sizes
+  const widthSize = useMemo(
+    () => () => {
+      switch (size) {
+        case 'xs':
+          return 96
+        case 'sm':
+          return 128
+        case 'base':
+          return 160
+        case 'lg':
+          return 192
+        case 'xl':
+          return 256
+        case 'xxl':
+          return 320
+        // case '3xl':
+        //   return 345
+        // case '4xl':
+        //   return 375
+        // case '5xl':
+        //   return 400
+        // case '6xl':
+        //   return 425
+        // case '7xl':
+        //   return 450
+        // case '8xl':
+        //   return 475
+        // case '9xl':
+        //   return 500
+        default:
+          return undefined
+      }
+    },
+    [size]
+  )
+
+  // aspect ratio
+  const aspectSize = useMemo(
+    () => () => {
+      switch (ratio) {
+        case '1:1':
+          return '100%'
+        case '3:2':
+          return '66.66%'
+        case '4:3':
+          return '75%'
+        case '8:5':
+          return '62.5%'
+        case '16:9':
+          return '56.25%'
+        default:
+          return undefined
+      }
+    },
+    [ratio]
+  )
+
+  const ImageWrapper = ({ children }: any) => (
+    <div className={containerCls} style={{ paddingTop: aspectSize() }}>
+      <div className={overlayCls}>{children}</div>
+    </div>
+  )
+
+  const image = (
     <Img
-      className={cn('Image', className)}
+      className={cn(imgCls, fitCls[fit])}
       alt={alt}
       src={src}
       loader={loader}
       unloader={unloader}
     />
+  )
+
+  const viewerWidthStyles: React.CSSProperties = {
+    maxWidth: width ? width : widthSize(),
+    height: height ? height : undefined,
+    backgroundColor: background === 'default' ? '#f7fbfd' : '',
+  }
+
+  const baseStyles = cn(
+    'Image',
+    baseCls,
+    roundedCls[rounded],
+    backgroundCls[background],
+    { [borderCls]: border === true },
+    className
+  )
+
+  return (
+    <div className={baseStyles} style={viewerWidthStyles}>
+      {!height ? <ImageWrapper>{image}</ImageWrapper> : image}
+    </div>
   )
 }
 
--- src/components/Image/types.ts
+++ src/components/Image/types.ts
@@ -1,7 +1,14 @@
 import { ImgProps } from 'react-image';
+import { Colors } from '../../utils/common_enums';
 
 export interface IImageProps extends ImgProps {
   className?: string
-  // rounded?: boolean
-  // fit?: 'contain' | 'cover'
+  border?: boolean
+  fit?: 'contain' | 'cover'
+  size?: 'xxs' | 'xs' | 'sm' | 'base' | 'lg' | 'xl' | 'xxl'
+  rounded?: 'unset' | 'rounded' | 'full'
+  ratio?: '1:1' | '3:2' | '4:3' | '8:5' | '16:9'
+  width?: string | number
+  height?: string | number
+  background?: keyof typeof Colors
 }
