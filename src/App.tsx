412afeaf957b43944040c6d78ae0613aae0f11c2
Marnien Cueba <marnien@dnamicro.com>
2022-03-22 17:04:00 +0800

    App: enhancement

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
 import React, { useContext } from 'react'
 import cn from 'classnames'
 import { ThemeContext } from './context/ThemeContext'
-import profileImage from './assets/img/MarnienCueva.png'
 import { Icon, Image, Spinner } from './components'
 import LandingPage from './common/LandingPage'
+import bgdesign from './assets/img/design-bg.png'
 
 function App() {
   const {
     theme: {
-      appStyle: { baseCls, overlayCls, overlayDesignCls, containerCls },
+      appStyle: { baseCls, containerCls },
     },
   } = useContext(ThemeContext)
 
   return (
     <div className={cn('', baseCls)}>
-      <div className={overlayCls}>
-        <div className={overlayDesignCls}></div>
+      <div className='absolute inset-0'>
+        <Image
+          className='ml-auto'
+          border={false}
+          alt='alt-img'
+          src={bgdesign}
+          size='xxl'
+          width={'100%'}
+          height={'100%'}
+          fit='cover'
+          ratio='16:9'
+          rounded='unset'
+          loader={<Spinner />}
+          unloader={<Icon />}
+        />
       </div>
       <div className={containerCls}>
         <LandingPage />
       </div>
-      {/* <h1>Hello World, MarNi!</h1>
-      <Image
-        className='w-24 h-24'
-        src={'s'}
-        // src={profileImage}
-        loader={<Spinner />}
-        unloader={<Icon />}
-      /> */}
     </div>
   )
 }
