736950ea7a1775b16bd8ed93f094fd1f9bebf416
Marnien Cueba <marnien@dnamicro.com>
2022-03-22 17:01:29 +0800

    extend theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    borderRadius: {
-      'xl': '1.5rem'
+    extend: {
+      colors: {
+        primary: {
+          50: '#E5E9F4',
+          100: '#D7DDED',
+          200: '#C0C9E4',
+          300: '#9CAAD4',
+          400: '#647ABC',
+          DEFAULT: '#3D59AB',
+          600: '#314789',
+          700: '#27396E',
+          800: '#1F2E58',
+          900: '#192546',
+        }
+      }
     },
-    extend: {},
   },
   plugins: [],
-}
+} // ff004b
